d71f1e7fbae639cb533e5f1e8974647478a658c6
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-03-16 12:39:40 +0300

    add logs to the output

--- src/test/java/ShouldFail.java
+++ src/test/java/ShouldFail.java
@@ -8,9 +8,20 @@ import static org.testng.Assert.fail;
  */
 public class ShouldFail {
 
-
   @Test(retryAnalyzer = MyRetry.class)
   public void test1() {
     fail("Non-conditional failure");
+  }
+
+  @Test
+  public void test2(){
+    System.err.println("line1");
+    System.err.println("line2");
+    System.err.println("line3");
+    System.err.println("line4");
+    System.err.println("line5");
+    System.err.println("line6");
+    System.err.println("line7");
+    System.err.println("line8");
   }
 }
